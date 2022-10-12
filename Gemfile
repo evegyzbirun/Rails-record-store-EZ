96b607d7fbdfd69827f888ad82665c9513805630
evegyzbirun <zbirunzbirun@gmail.com>
2022-10-11 20:23:15 -0700

    fixed controller rb

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ group :development, :test do
   gem 'launchy'
   gem 'pry'
   gem 'dotenv-rails'
+  gem 'shoulda-matchers'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_022540) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_023850) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,4 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_12_022540) do
     t.string "genre"
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.string "lyrics"
+    t.integer "album_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "songs", "albums"
 end
